feb604ac6b1040dfe8637a69cda99e97b8ba9d8c
your_name <tetianabeliak2024@gmail.com>
2024-08-23 12:08:58 +0300

    1

---
+++ js/task-1.js
@@ -0,0 +1,15 @@
+function makeTransaction(quantity, pricePerDroid) {
+    return(`You ordered ${quantity} droids worth ${totalPrice = quantity * pricePerDroid} credits!`);
+}
+console.log(makeTransaction(5, 3000));
+console.log(makeTransaction(3, 1000));
+console.log(makeTransaction(10, 500));
+
+
+
+
+
+
+
+
+
